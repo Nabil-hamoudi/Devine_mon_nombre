4870b99c6d3b4dc0e69b9e1fc4d0664be713bed9
Nabil-hamoudi <nabil.hamoudi@ens.uvsq.fr>
2022-11-06 16:33:51 +0100

    exo 4

--- src/devine_mon_nombre.rs
+++
@@ -1,9 +0,0 @@
-use std::io;
-
-fn main() {
-    println!("Devine mon nombre!");
-    let mut mess = String::new();
-    let num = io::stdin().read_line(&mut mess);
-    println!("{}", num.unwrap());
-    println!("{mess}")
-}
